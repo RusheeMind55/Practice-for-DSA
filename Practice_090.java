da9af40124d16fb3ce4c78aef51f1644060dd15e
Rushikesh Mind <157093618+RusheeMind55@users.noreply.github.com>
2024-03-30 18:28:13 +0530

    Swapping Two Number

--- Practice_090.java
+++ Practice_090.java
@@ -6,27 +6,19 @@ public class Practice_090 {
 	
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+	
+		int a=45;
+		int b=75;
 		
-		System.out.println("Eneter First Number : ");
-		int a=sc.nextInt();
-		
-		System.out.println("Eneter Second Number : ");
-		int b=sc.nextInt();
-		
-		System.out.println("Eneter Third Number : ");
-		int c=sc.nextInt();
-		
-		if(a>b && a>c) {
-			System.out.println("a is the largest number : " +a);
-		}
-		else if(b>a && b>c) {
-			System.out.println("b is the largest number : " +b);	
-		}
-		else {
-			System.out.println(" C is the largest number :" +c);
+		System.out.println("before swapping"+a+""+b);
+	
+		int t=a;
+		a=b;
+		b=t;
+		System.out.println(a+""+b);
 		}
 		
 		
-	}
+	
 
 }
